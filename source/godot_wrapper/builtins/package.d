61ce40a760f8b4f9136e447c89cbc92fb6603fdc
castalien-nakamura <nakamura.akira@castalien.co.jp>
2024-05-18 16:21:23 +0900

    add variant type

--- source/godot_wrapper/builtins/package.d
+++ source/godot_wrapper/builtins/package.d
@@ -3,4 +3,5 @@ module godot_wrapper.builtins;
 public
 {
     import godot_wrapper.builtins.string_name;
+    import godot_wrapper.builtins.variant;
 }
---
+++ source/godot_wrapper/builtins/variant.d
@@ -0,0 +1,10 @@
+module godot_wrapper.builtins.variant;
+
+/** 
+Godot Variant type.
+*/
+struct GodotVariant
+{
+private:
+    ubyte[40] internal_;
+}
